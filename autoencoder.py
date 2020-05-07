520ca33808442d57821b77b52b1c553ad6b16ed6
jaejeon <jaejeon@student.42seoul.kr>
2020-05-07 17:31:53 +0900

    Modify: Autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -62,51 +62,53 @@ def train(loss, model, opt, original):
 
 
 def train_loop(model, opt, loss, dataset, epochs):
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         epoch_loss = 0
-        for step, batch_featrues in enumerate(dataset):
+        for step, batch_featrues in enumerate(tqdm(dataset)):
             loss_values = train(loss, model, opt, batch_featrues)
             epoch_loss += loss_values
         model.loss.append(epoch_loss)
         print('Epoch {}/{}. Loss: {}'.format(epoch + 1, epochs, epoch_loss.numpy()))
 
 
-
-
-
-def run(tag_to_id_fname, id_to_tag_fname, train_fname, question_fname):
+def run(tag_to_id_fname, id_to_tag_fname, train_fname, test_fname):
     print("Loading tag_to_id...")
     tag_to_id = load_json(tag_to_id_fname)
     print("Loading id_to_tag...")
     id_to_tag = load_json(id_to_tag_fname)
     print("Loading train file...")
-    mlb_songs = MultiLabelBinarizer(classes=np.arange(707989), sparse_output=True)
-    mlb_tags = MultiLabelBinarizer(classes=np.arange(30653), sparse_output=True)
     train_data = load_json(train_fname)
     for ply in train_data:
         ply['tags'] = [tag_to_id[tag] for tag in ply['tags']]
-    # print("Loading question file...")
-    # questions = load_json(question_fname)
-    # for ply in questions:
-    #     ply['tags'] = [tag_to_id[tag] for tag in ply['tags']]
-    # test_songs = mlb_songs.fit_transform([ply['songs'] for ply in questions])
-    # test_tags = mlb_tags.fit_transform([ply['tags'] for ply in questions])
-    # x_test = [tf.concat([songs, tags], 1) for songs, tags in zip(test_songs, test_tags)]
+
+    print("Loading test file...")
+    test_data = load_json(test_fname)
+    for ply in test_data:
+        ply['tags'] = [tag_to_id[tag] for tag in ply['tags']]
     # print("Writing answers...")
     # answers = self._generate_answers(song_meta_json, train_data, questions)
     # write_json(answers, "results/results.json")
     print("Make Training dataset...")
 
-    def generator():
+    def train_generator():
         for x in train_data:
             songs = np.zeros(707989)
             tags = np.zeros(30653)
-            for s in x['songs']:
-                songs[s] = 1
-            for t in x['tags']:
-                tags[t] = 1
+            songs[x['songs']] = 1
+            tags[x['tags']] = 1
             yield np.concatenate([songs, tags])
-    training_dataset = tf.data.Dataset.from_generator(generator=generator, output_types=tf.float32, output_shapes=tf.TensorShape([707989+30653])).batch(256) #tf.data.Dataset.from_tensor_slices(x_train).batch(256)
+
+    def test_generator():
+        for x in test_data:
+            songs = np.zeros(707989)
+            tags = np.zeros(30653)
+            songs[x['songs']] = 1
+            tags[x['tags']] = 1
+            yield np.concatenate([songs, tags])
+    training_dataset = tf.data.Dataset.from_generator(generator=train_generator, output_types=tf.float32,
+                                                      output_shapes=tf.TensorShape([707989+30653])).batch(256)
+    test_dataset = tf.data.Dataset.from_generator(generator=test_generator, output_types=tf.float32,
+                                                  output_shapes=tf.TensorShape([707989+30653])).batch(256)
 
     model = AutoEncoder(intermediate_dim=128, original_dim=707989+30653)
     opt = tf.keras.optimizers.Adam(learning_rate=1e-2)
